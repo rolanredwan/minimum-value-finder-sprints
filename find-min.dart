a50b619427e3949a33187b6a0e686a2c853326aa
rolanredwan <106884918+rolanredwan@users.noreply.github.com>
2026-01-13 10:45:09 +0200

    Implement findMin function to get minimum value

--- find-min.dart
+++ find-min.dart
@@ -1,1 +1,16 @@
+void main() {
+  // Task4:
+  var numList = [20, 93, 12, 32, 11, 2, 43];
+  print("The minimum number in the list is ${findMin(numList)}");
+}
+
+int findMin(var numList) {
+  int minNum = numList[0];
+  for (int i = 0; i < numList.length; i++) {
+    if (numList[i] < minNum) {
+      minNum = numList[i];
+    }
+  }
+  return minNum;
+}
 
